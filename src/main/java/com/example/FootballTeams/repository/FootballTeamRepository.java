fc21b41d7a59c4a6e256313a9b8be0cea09a5965
RadmiloM <markovicradmilo35@gmail.com>
2023-01-05 20:12:57 +0100

    football teams repository created

---
+++ src/main/java/com/example/FootballTeams/repository/FootballTeamRepository.java
@@ -0,0 +1,7 @@
+package com.example.FootballTeams.repository;
+
+import com.example.FootballTeams.entity.FootballTeam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FootballTeamRepository extends JpaRepository<FootballTeam,Integer> {
+}
