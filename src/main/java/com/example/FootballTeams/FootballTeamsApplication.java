9462e662aa9cf3a78dd8ec3e2185a55f9abdc83b
RadmiloM <markovicradmilo35@gmail.com>
2023-01-05 19:13:42 +0100

    First commit on github for football application related to class which starts the application

---
+++ src/main/java/com/example/FootballTeams/FootballTeamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.FootballTeams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTeamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTeamsApplication.class, args);
+	}
+
+}
