98d5350ed550bc3895f9dd7c30dc66450ecd9d7f
RadmiloM <markovicradmilo35@gmail.com>
2023-01-05 20:11:19 +0100

    football teams table created and data inserted

---
+++ src/main/resources/db/migration/V1.1__create_football_team_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS football_teams (
+id SERIAL PRIMARY KEY,
+team VARCHAR,
+player_name VARCHAR,
+position VARCHAR,
+skill_level INTEGER,
+price INTEGER
+);
---
+++ src/main/resources/db/migration/V1.2__insert_into_football_team.sql
@@ -0,0 +1,7 @@
+INSERT INTO football_teams (team,player_name,position,skill_level,price) VALUES ('Chelsea','Mason Mount','AM',87,45000000);
+INSERT INTO football_teams (team,player_name,position,skill_level,price) VALUES ('Juventus','Dusan Vlahovic','ST',92,87000000);
+INSERT INTO football_teams (team,player_name,position,skill_level,price) VALUES ('PSG','Lionel Messi','AM',95,60000000);
+INSERT INTO football_teams (team,player_name,position,skill_level,price) VALUES ('Fulham','Aleksandar Mitrovic','ST',93,35000000);
+INSERT INTO football_teams (team,player_name,position,skill_level,price) VALUES ('Arsenal','Bukayo Saka','RW',90,8000000);
+INSERT INTO football_teams (team,player_name,position,skill_level,price) VALUES ('Liverpool','Mohamed Salah','RW',90,75000000);
+INSERT INTO football_teams (team,player_name,position,skill_level,price) VALUES ('Real Madrid','Vinicius Junior','LW',92,9000000);
