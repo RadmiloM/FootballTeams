7cd72440bd8d95cddbc39715e8a827b25e4ce6d1
RadmiloM <markovicradmilo35@gmail.com>
2023-01-05 20:19:10 +0100

    football team entity created

---
+++ src/main/java/com/example/FootballTeams/entity/FootballTeam.java
@@ -0,0 +1,25 @@
+package com.example.FootballTeams.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name= "football_teams")
+public class FootballTeam {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String team;
+
+    private String playerName;
+
+    private String position;
+
+    private Integer skillLevel;
+
+    private Integer price;
+}
