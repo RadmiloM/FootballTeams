70d5f7c1c57a51e5f730f079ad3b99e8b3a2c3cd
RadmiloM <markovicradmilo35@gmail.com>
2023-01-09 20:17:32 +0100

    FootballTeamRepository findAllByTeam method created.

--- src/main/java/com/example/FootballTeams/repository/FootballTeamRepository.java
+++ src/main/java/com/example/FootballTeams/repository/FootballTeamRepository.java
@@ -1,9 +1,15 @@
 package com.example.FootballTeams.repository;
 
 import com.example.FootballTeams.entity.FootballTeam;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FootballTeamRepository extends JpaRepository<FootballTeam, Integer> {
 
     Boolean existsByPlayerName(String name);
+
+    Page<FootballTeam> findAllByTeam(String team, Pageable pageable);
+
+
 }
