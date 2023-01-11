353e48b0f316f4ec0e606d5f0889ceabdab968c3
RadmiloM <markovicradmilo35@gmail.com>
2023-01-11 22:59:05 +0100

    annotation @Repository added to repository classes.

--- src/main/java/com/example/FootballTeams/repository/FootballTeamRepository.java
+++ src/main/java/com/example/FootballTeams/repository/FootballTeamRepository.java
@@ -4,7 +4,9 @@ import com.example.FootballTeams.entity.FootballTeam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FootballTeamRepository extends JpaRepository<FootballTeam, Integer> {
 
     Boolean existsByPlayerName(String name);
