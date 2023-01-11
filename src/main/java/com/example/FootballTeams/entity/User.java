73720b2312a4036e4e8a822d4cc60a46a66c98d2
RadmiloM <markovicradmilo35@gmail.com>
2023-01-11 22:51:01 +0100

    Created all args constructor and no args constructor in User class.

--- src/main/java/com/example/FootballTeams/entity/User.java
+++ src/main/java/com/example/FootballTeams/entity/User.java
@@ -1,6 +1,9 @@
 package com.example.FootballTeams.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -8,6 +11,8 @@ import java.time.LocalDateTime;
 @Entity
 @Data
 @Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
